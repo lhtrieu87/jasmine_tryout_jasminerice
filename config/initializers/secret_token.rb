d86bf74144409998270f2d22cf6e1bacfff7f322
darksteelgames <darksteelgames@darksteelgamess-Mac-mini.local>
2013-09-14 11:49:14 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JasmineTryOut::Application.config.secret_key_base = 'bcba5af8b8784098010ddd9dc3ca673217fc93ad0f80ade793c06f778348136732c6c3781cf7e67951b88eb57b6988259f2efc103a9c89d9fb9913e0fa4fe3ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JasmineTryOut::Application.config.session_store :cookie_store, key: '_jasmine_try_out_session'
